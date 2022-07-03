7d3911c06d171384ff33bcbffa6dd1aa36e3eb79
Ayan Banerjee <108017943+AyanBanerjee280@users.noreply.github.com>
2022-07-03 13:15:45 +0530

    Add files via upload

---
+++ Write-only_Property.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Encapsulation_with_Csharp
+{//Write-only property
+    class Account
+    {
+        int AccountBalance = 4;
+        public int Balance
+        {
+            set
+            {
+                if(value < 0)
+                {
+                    Console.WriteLine("Negative values cannot be passed.");
+                }
+                else
+                {
+                    AccountBalance = value;
+                }
+            }
+            
+        }
+    }
+    class get_set_method
+    {
+        static void Main(string[] args)
+        {
+            Account obj = new Account();
+            obj.Balance = -55;
+            
+            Console.ReadLine();
+        }
+    }
+}
