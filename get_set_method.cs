0d43f214a4f95444374fba9204618edf0541369c
Ayan Banerjee <108017943+AyanBanerjee280@users.noreply.github.com>
2022-07-03 13:14:02 +0530

    Add files via upload

---
+++ get_set_method.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace Encapsulation_with_Csharp
+{//Read Only Property of Encapsulation
+    class Account
+    {
+        int AccountBalance = 40000;
+        public int Balance
+        {
+            set
+            {
+                if(value < 0)
+                {
+                    Console.WriteLine("Negative values cannot be passed.");
+                }
+                else
+                {
+                    AccountBalance = value;
+                }
+            }
+            get
+            {
+                return AccountBalance;
+            }
+        }
+    }
+    class Read_Only_Property
+    {
+        static void Main(string[] args)
+        {
+            Account obj = new Account();
+            obj.Balance = -55;
+            Console.WriteLine("The balance in your account is " + obj.Balance);
+            Console.ReadLine();
+        }
+    }
+}
