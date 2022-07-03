ba29a0e7f8ca308b8a8ac4c4ee48c5209c190e3a
Ayan Banerjee <108017943+AyanBanerjee280@users.noreply.github.com>
2022-07-03 13:06:39 +0530

    Add files via upload

---
+++ Encapsultaion_with_Csharp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Encapsulation_with_Csharp
+{//Encapsulate data with GetBalance and SetBalance method
+    class Account
+    {
+        int AccountBalance = 100;
+        public void setBalance(int Amount)
+        {
+            if(Amount < 0)
+            {
+                Console.WriteLine("Negative values cannot be assigned.");
+            }
+            else
+            {
+                AccountBalance = Amount;
+            }
+        }
+        public void getBalance()
+        {
+            Console.WriteLine("The balance in your account is " + AccountBalance + ".");
+        }
+    }
+    class Encapsultaion_with_Csharp
+    {
+        static void Main(string[] args)
+        {
+            Account obj = new Account();
+            obj.setBalance(4500);
+            obj.getBalance();
+            Console.ReadLine();
+        }
+    }
+}
