9f0618de88bcaa5a40bae9daed05f0a81dc598ac
Ayan Banerjee <108017943+AyanBanerjee280@users.noreply.github.com>
2022-07-03 13:11:25 +0530

    Add files via upload

---
+++ Read_Only_Property.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Encapsulation_with_Csharp
+{//Read Only Property of Encapsulation
+    class Account
+    {
+        int AccountBalance = 40000;
+        public int Balance
+        {
+            get
+            {
+                return AccountBalance;
+            }
+        }
+    }
+    class Encapsultaion_with_Csharp2
+    {
+        static void Main(string[] args)
+        {
+            Account obj = new Account();
+            Console.WriteLine("The balance in your account is " + obj.Balance);
+            Console.ReadLine();
+        }
+    }
+}
