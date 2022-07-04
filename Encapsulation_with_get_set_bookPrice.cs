a14a7fdd363da46a48efda09f01229e274dd9768
Ayan Banerjee <108017943+AyanBanerjee280@users.noreply.github.com>
2022-07-04 10:43:32 +0530

    Add files via upload

---
+++ Encapsulation_with_get_set_bookPrice.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace Encapsulation
+{
+    class BookPrice
+    {
+        int price = 0;
+        public int bookPrice
+        {
+            set
+            {
+                if(value < 0)
+                {
+                    Console.WriteLine("Negative values cannot be passed.");
+                }
+                else
+                {
+                    price = value;
+                }
+            }
+            get
+            {
+                return price;
+            }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BookPrice obj = new BookPrice();
+            obj.bookPrice = 500;
+            Console.WriteLine("The price for the selected book is " + obj.bookPrice);
+            Console.ReadLine();
+        }
+           
+    }
+}
