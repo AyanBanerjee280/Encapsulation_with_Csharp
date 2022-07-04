5d513ffccfc3e577083a6c8ec2f94af847d26046
Ayan Banerjee <108017943+AyanBanerjee280@users.noreply.github.com>
2022-07-04 10:38:19 +0530

    Add files via upload

---
+++ Encapsulation_with_car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Encapsulation
+{
+    class car
+    {
+        int mileage = 0;
+        public void setMileage(int km)
+        {
+
+            if (km < 15)
+            {
+                Console.WriteLine("Mileage is too low.");
+            }
+            else
+            {
+                mileage = km;
+            }
+        }
+        public void getMileage()
+        {
+            Console.WriteLine("The mileage of this car is " + mileage + " kmpl.");
+        }
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            car obj = new car();
+            obj.setMileage(21);
+            obj.getMileage();
+            Console.ReadLine();
+        }
+    }
+}
